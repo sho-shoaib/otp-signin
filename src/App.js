5c9d53c808076355bf0dc0eb2d511fa7ed9c022c
Shoaib Shaikh <shaikhsho292@gmail.com>
2022-09-16 20:19:42 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+import { signInWithPhoneNumber } from "firebase/auth";
 
 function App() {
+  const [mobile, setMobile] = useState(0);
+
+  const auth = getAuth();
+  window.recaptchaVerifier = new RecaptchaVerifier(
+    "sign-in-button",
+    {
+      size: "invisible",
+      callback: (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        onSignInSubmit();
+        console.log("Recaptca verified");
+      },
+    },
+    auth
+  );
+
+  const onSignInSubmit = (e) => {
+    e.preventDefault();
+    const phoneNumber = mobile;
+    const appVerifier = window.recaptchaVerifier;
+
+    const auth = getAuth();
+    signInWithPhoneNumber(auth, phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col gap-5 p-5'>
+      <h2 className='text-3xl font-semibold'>Login Form</h2>
+
+      <div>
+        <form action='' onSubmit={() => onSignInSubmit()}>
+          <div id='sign-in-button'></div>
+          <div className='flex gap-5'>
+            <input
+              type='number'
+              name='mobile'
+              placeholder='Mobile Number'
+              required
+              className='w-92 p-2 bg-purple-200 border-2 border-purple-600 rounded-md'
+              onChange={(e) => setMobile(e.target.value)}
+            />
+            <button
+              type='submit'
+              className='px-4 py-2 text-lg bg-purple-600 text-white rounded-lg border-0 shadow-md hover:shadow-lg transition hover:scale-105 duration-300 active:scale-100 active:shadow-md'
+            >
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCm_2QDoJ69wMmKfqqnO-KEEVT9W8hS4zU",
+  authDomain: "uorfinft-1f57e.firebaseapp.com",
+  projectId: "uorfinft-1f57e",
+  storageBucket: "uorfinft-1f57e.appspot.com",
+  messagingSenderId: "135156237925",
+  appId: "1:135156237925:web:cfe209d0c0e99e0e79688c",
+  measurementId: "G-TS3EHB0Z2P",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
